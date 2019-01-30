fd0704111b205287787aacd075028646649650f5
wafaagamal <wafaagamal>
2019-01-30 18:05:58 +0200

    fix

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ function start(url,method,ticket,body) {
     let Admin = await start(url,'POST',null,regObject)
     console.log(Admin,"===========================activate-Admin ############################");
 
-     await delay(1000);
+     //await delay(1000);
     url='/stage/supervisor'
     supervisor=generate.staff()
     regObject = await start(url,'POST',Admin.ticket,supervisor)
@@ -137,127 +137,127 @@ function start(url,method,ticket,body) {
     console.log(Supervisor,"===========================activate-supervisor############################");
 
     //  await delay(1000);
-    url='/stage/driver'
-    regObject = await start(url,'POST',Supervisor.ticket,generate.driver())
+//     url='/stage/driver'
+//     regObject = await start(url,'POST',Supervisor.ticket,generate.driver())
     
-    regObject.password=helper.generate('mix', 8)
-    regObject.mobileNumber="010"+helper.generate('numeric', 8)
-    formData = {
-        'image': fs.createReadStream('./image.jpg'),
-        'email': regObject.email,
-        'password':regObject.password,
-        'mobileNumber':regObject.mobileNumber,
-        'regCode':regObject.regCode,
-     };
-    //  await delay(1000);
-    url='/activate/driver'
-    let _driver=  await activeDriver(url,'POST',null,formData)
+//     regObject.password=helper.generate('mix', 8)
+//     regObject.mobileNumber="010"+helper.generate('numeric', 8)
+//     formData = {
+//         'image': fs.createReadStream('./image.jpg'),
+//         'email': regObject.email,
+//         'password':regObject.password,
+//         'mobileNumber':regObject.mobileNumber,
+//         'regCode':regObject.regCode,
+//      };
+//     //  await delay(1000);
+//     url='/activate/driver'
+//     let _driver=  await activeDriver(url,'POST',null,formData)
    
- console.log(_driver.user._id,"=========================== DRIVER ID ############################");
+//  console.log(_driver.user._id,"=========================== DRIVER ID ############################");
  
- await delay(1000);
-    url='/create/rider'
-    rider=generate.rider()
-    let riders=await start(url,'POST',null,rider)
+//  await delay(1000);
+//     url='/create/rider'
+//     rider=generate.rider()
+//     let riders=await start(url,'POST',null,rider)
     
-   console.log(riders,"===================================CREATE #############################################");
+//    console.log(riders,"===================================CREATE #############################################");
 
-   await delay(1000);
-    url='/access/rider'
-    let _rider=await start(url,'POST',null,rider)
-    console.log(_rider,"==========================ACCESS #############################################");
+//    await delay(1000);
+//     url='/access/rider'
+//     let _rider=await start(url,'POST',null,rider)
+//     console.log(_rider,"==========================ACCESS #############################################");
 
 
-if(_rider){
+// if(_rider){
 
-    // await delay(1000);
-     url='/location'
-     console.log(obj,"center***************");
-     let bg=helper.generateBgLocation(obj,300)
-     let bgRider=await start(url,'POST',_rider.ticket,bg)
+//     // await delay(1000);
+//      url='/location'
+//      console.log(obj,"center***************");
+//      let bg=helper.generateBgLocation(obj,300)
+//      let bgRider=await start(url,'POST',_rider.ticket,bg)
     
-     console.log(bgRider,"========================= BG LOCATION RIDER #########################");
-if(bgRider){
-    // await delay(1000);
-    bg=helper.generateBgLocation(obj,500)
-  let bgDriver=await start(url,'POST',_driver.ticket,bg)
+//      console.log(bgRider,"========================= BG LOCATION RIDER #########################");
+// if(bgRider){
+//     // await delay(1000);
+//     bg=helper.generateBgLocation(obj,500)
+//   let bgDriver=await start(url,'POST',_driver.ticket,bg)
 
-  console.log(bgDriver,"========================= BG LOCATION DRIVER #########################");
-}
-  await delay(1000);
-    url='/user/push'
-    let pushId="bk3RNwTe3H0:CI2k_HHwgIpoDKCIZvvDMExUdFQ3P1bk3RNwTe3H0:CI2kbk3RNwTe3H0:CI2k_HHwgIpoDKCIZvvDMExUdFQ3P1bk3RNwTe3H0:CI2k"
-    let rider_id=await start(url,'POST',_rider.ticket,{'pushId':pushId})
-    console.log(rider_id,"========================= PUSH ID RIDER #########################");
+//   console.log(bgDriver,"========================= BG LOCATION DRIVER #########################");
+// }
+//   await delay(1000);
+//     url='/user/push'
+//     let pushId="bk3RNwTe3H0:CI2k_HHwgIpoDKCIZvvDMExUdFQ3P1bk3RNwTe3H0:CI2kbk3RNwTe3H0:CI2k_HHwgIpoDKCIZvvDMExUdFQ3P1bk3RNwTe3H0:CI2k"
+//     let rider_id=await start(url,'POST',_rider.ticket,{'pushId':pushId})
+//     console.log(rider_id,"========================= PUSH ID RIDER #########################");
 
-    let driver_id=await start(url,'POST',_driver.ticket,{'pushId':pushId})
-    console.log(driver_id,"========================= PUSH ID DRIVER #########################");
+//     let driver_id=await start(url,'POST',_driver.ticket,{'pushId':pushId})
+//     console.log(driver_id,"========================= PUSH ID DRIVER #########################");
   
-   url='/add/car'
-   let car={
-    'uniqueId':helper.generate('numeric',6),
-    'desc':helper.generate("lalpha",30)
-   }
-    let carId=await start(url,'POST',Supervisor.ticket,car)
-    console.log(carId,"========================= CREATE CAR #########################");
+//    url='/add/car'
+//    let car={
+//     'uniqueId':helper.generate('numeric',6),
+//     'desc':helper.generate("lalpha",30)
+//    }
+//     let carId=await start(url,'POST',Supervisor.ticket,car)
+//     console.log(carId,"========================= CREATE CAR #########################");
 
-    await delay(1000);
-    url='/car/driver'
-    let msg=await start(url,'POST',_driver.ticket,carId)   
-    console.log(msg,"========================= CONNECT DRIVER TO CAR #########################");
+//     await delay(1000);
+//     url='/car/driver'
+//     let msg=await start(url,'POST',_driver.ticket,carId)   
+//     console.log(msg,"========================= CONNECT DRIVER TO CAR #########################");
  
-    await delay(1000);
-    url='/journey'
-    let jour=await start(url,'POST',_rider.ticket,coords)
-    console.log(jour,"========================= CREATE JOURNEY #########################");
+//     await delay(1000);
+//     url='/journey'
+//     let jour=await start(url,'POST',_rider.ticket,coords)
+//     console.log(jour,"========================= CREATE JOURNEY #########################");
 
-    await delay(1000);
-    url='/journey/latest'
-    let last=await start(url,'GET',_rider.ticket)
-    console.log(last.journey._id,"========================= GET LAST JOURNEY #########################");
+//     await delay(1000);
+//     url='/journey/latest'
+//     let last=await start(url,'GET',_rider.ticket)
+//     console.log(last.journey._id,"========================= GET LAST JOURNEY #########################");
     
     
-     await delay(2000);
-    url='/journey/accept'
-    let accept=await start(url,'POST',_driver.ticket,{"journeyId":last.journey._id})
-    console.log(accept.journey._id,"========================= ACCEPT JOURNEY #########################");
+//      await delay(2000);
+//     url='/journey/accept'
+//     let accept=await start(url,'POST',_driver.ticket,{"journeyId":last.journey._id})
+//     console.log(accept.journey._id,"========================= ACCEPT JOURNEY #########################");
  
 
-    await delay(1000);
-    url='/journey/start'
-    let startJ=await  start(url,'POST',_driver.ticket,{"riderCode":accept.journey.riderCode})
-    console.log(startJ,"========================= START JOURNEY #########################");
+//     await delay(1000);
+//     url='/journey/start'
+//     let startJ=await  start(url,'POST',_driver.ticket,{"riderCode":accept.journey.riderCode})
+//     console.log(startJ,"========================= START JOURNEY #########################");
     
-    for(let c=0; c<10000; c++){
-      console.log("hereeeeeeeeeeeeeeeeeeeee");
-      url='/location'
-      let bg=await start(url,'POST',_driver.ticket,helper.generateBgLocation(obj,500))
-      console.log(bg,"========================= BG LOCATION  #########################");
-      await delay(1000);
-    }
+//     for(let c=0; c<10000; c++){
+//       console.log("hereeeeeeeeeeeeeeeeeeeee");
+//       url='/location'
+//       let bg=await start(url,'POST',_driver.ticket,helper.generateBgLocation(obj,500))
+//       console.log(bg,"========================= BG LOCATION  #########################");
+//       await delay(1000);
+//     }
    
-    // let time=setInterval(async function(){
-    // console.log("HEREEEEE**************************************************");
-    //       url='/location'
-    //       let bg=await start(url,'POST',_driver.ticket,helper.generateBgLocation(obj,500))
-    //       console.log(bg,"========================= BG LOCATION  #########################");
-    //       if(end){
-    //       console.log("============================CLEAR INTERVAL======================");
-    //         clearInterval(time)
-    //       }
-    // },10000)
+//     // let time=setInterval(async function(){
+//     // console.log("HEREEEEE**************************************************");
+//     //       url='/location'
+//     //       let bg=await start(url,'POST',_driver.ticket,helper.generateBgLocation(obj,500))
+//     //       console.log(bg,"========================= BG LOCATION  #########################");
+//     //       if(end){
+//     //       console.log("============================CLEAR INTERVAL======================");
+//     //         clearInterval(time)
+//     //       }
+//     // },10000)
       
-setTimeout(async function(){
-    url='/journey/end'
-    end=await start(url,'POST',_driver.ticket,{"riderCode":startJ.journey.riderCode})
-     console.log(end.journey.cost,"%%%%%%%%%%%%%%%%%%%%%%JOURNNEY END%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-         arr[end.journey._id] =end.journey.cost
-         fs.appendFile('history.json',`{${JSON.stringify(arr)} ,"prossesID": ${process.pid}}`+','+'\r\n')
-         console.log(arr,"ARRAYYYYYY_____________________________==###");
+// setTimeout(async function(){
+//     url='/journey/end'
+//     end=await start(url,'POST',_driver.ticket,{"riderCode":startJ.journey.riderCode})
+//      console.log(end.journey.cost,"%%%%%%%%%%%%%%%%%%%%%%JOURNNEY END%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+//          arr[end.journey._id] =end.journey.cost
+//          fs.appendFile('history.json',`{${JSON.stringify(arr)} ,"prossesID": ${process.pid}}`+','+'\r\n')
+//          console.log(arr,"ARRAYYYYYY_____________________________==###");
          
-    },1000*10000)
+//     },1000*10000)
 
-  }//  
+  //}//  
   }catch(err) {
     console.log("####################################ERROR",err)
   }
