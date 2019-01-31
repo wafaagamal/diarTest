3b6ade6f3e740902ccdccbc5e7e92ef5c2f5aa1e
wafaagamal <wafaagamal>
2019-01-31 19:28:31 +0200

    fix

--- app.js
+++ app.js
@@ -241,7 +241,7 @@ setTimeout(async function(){
   }
 
 }
-  async.times(3, main, function(result){
+  async.times(2, main, function(result){
 
     console.log("RESULT############################");	
     if (process.pid) {
