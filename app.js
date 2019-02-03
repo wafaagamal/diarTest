390932c58d7c8aef2c96c4a5c1b2a698241689b3
wafaagamal <wafaagamal>
2019-02-03 15:44:00 +0200

    fix

--- app.js
+++ app.js
@@ -163,7 +163,7 @@ console.log(bgDriver,"========================= BG LOCATION DRIVER #############
       url='/location'
       console.log(obj,"*******************Center RIDER***************");
       let b_g=helper.generateBgLocation(obj,300)
-      let bgRider=await start(url,'POST',_rider.ticket,b_g)
+      let bgRider= start(url,'POST',_rider.ticket,b_g)
       console.log(bgRider,"========================= BG LOCATION RIDER #########################");
       
     }) 
